940450197d93c713e12ce8834721111790d6c809
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-15 17:29:15 +0100

    specifications added

--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -6,17 +6,17 @@ const Slider = () => {
       <div className="absolute inset-0 bg-[url(https://res.cloudinary.com/dk3zfjoii/image/upload/v1715722594/robot_bg_e1ca025ed5.webp)] bg-cover bg-center bg-no-repeat brightness-50 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l "></div>
 
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-        <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <div className="max-w-xl text-left">
           <h1 className="text-white text-3xl font-extrabold sm:text-5xl">
             Türkiye&apos;nin Solar Panel Yıkama Robotu
-            <strong className="block font-extrabold text-primary bg-white w-6/12 py-4 bg-opacity-40 items-center justify-center m-auto">
+            <strong className="block font-extrabold text-primary bg-white w-6/12 py-4 bg-opacity-40 items-left justify-left">
               RoboSpace
             </strong>
           </h1>
 
-          <p className="mt-4 text-white max-w-lg sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+          <p className="mt-4 text-white max-w-lg sm:text-2xl/relaxed">
+            Temiz Panel <span className="text-white">Kazandırır</span>. %30 Daha Verimli Paneller.
+            
           </p>
 
           <div className="mt-8 flex flex-wrap gap-4 text-center">
--- app/_components/Specifications.jsx
+++ app/_components/Specifications.jsx
@@ -1,10 +1,16 @@
+import { FaFeatherAlt, FaPaintRoller, FaCarBattery, FaShieldAlt } from "react-icons/fa";
+import { FaArrowsLeftRightToLine } from "react-icons/fa6";
+import { MdOutlineSettingsRemote, MdCandlestickChart,MdOutlineRoute, MdTouchApp } from "react-icons/md";
+import { RiSpeedFill } from "react-icons/ri";
+import { GiTankTread, GiStickyBoot } from "react-icons/gi";
+
 const Specifications = () => {
   return (
     <section className="bg-primary text-white">
-      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:mx-auto lg:py-16">
         <div className="max-w-xl">
           <h2 className="text-3xl font-bold sm:text-4xl">
-            What makes us special
+            Öne Çıkan Özelliklerimiz
           </h2>
 
           <p className="mt-4 text-gray-300">
@@ -17,26 +23,11 @@ const Specifications = () => {
         <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
           <div className="flex items-start gap-4">
             <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
+              <FaFeatherAlt/>
             </span>
 
             <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h2 className="text-lg font-bold">Hafif Alüminyum Gövde</h2>
 
               <p className="mt-1 text-sm text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -48,26 +39,11 @@ const Specifications = () => {
 
           <div className="flex items-start gap-4">
             <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
+            <FaPaintRoller />
             </span>
 
             <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h2 className="text-lg font-bold">Elektrostatik Boya</h2>
 
               <p className="mt-1 text-sm text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -79,26 +55,11 @@ const Specifications = () => {
 
           <div className="flex items-start gap-4">
             <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
+            <FaArrowsLeftRightToLine />
             </span>
 
             <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h2 className="text-lg font-bold">50cm Aralığa Kadar Panelden Panele Geçişlere Uygun Mekanik Yapı</h2>
 
               <p className="mt-1 text-sm text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -110,26 +71,11 @@ const Specifications = () => {
 
           <div className="flex items-start gap-4">
             <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
+            <MdOutlineSettingsRemote />
             </span>
 
             <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h2 className="text-lg font-bold">75 Metreye Kadar Etkili Endüstriyel Uzaktan Kumanda</h2>
 
               <p className="mt-1 text-sm text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -141,26 +87,11 @@ const Specifications = () => {
 
           <div className="flex items-start gap-4">
             <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
+            <MdCandlestickChart />
             </span>
 
             <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h2 className="text-lg font-bold">Panel Yüzeylerini Çizmeden Hassas Temizlik Yapabilen İthal Fırçalar</h2>
 
               <p className="mt-1 text-sm text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -172,26 +103,97 @@ const Specifications = () => {
 
           <div className="flex items-start gap-4">
             <span className="shrink-0 rounded-lg bg-gray-800 p-4">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                ></path>
-              </svg>
+            <RiSpeedFill />
             </span>
 
             <div>
-              <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+              <h2 className="text-lg font-bold">Ayarlanabilir Yürüme ve Fırça Hız</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                cumque tempore est ab possimus quisquam reiciendis tempora
+                animi! Quaerat, saepe?
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+            <MdOutlineRoute />
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Eğimli Yüzeylerde Rotadan Şaşmadan Düz İlerlemeyi Sağlayan &quot;Rota Stabilizasyon Sistemi&quot;</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                cumque tempore est ab possimus quisquam reiciendis tempora
+                animi! Quaerat, saepe?
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+            <GiTankTread />
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Silikon Palet Yapısı ile Panellere Daha Yumuşak</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                cumque tempore est ab possimus quisquam reiciendis tempora
+                animi! Quaerat, saepe?
+              </p>
+            </div>
+          </div><div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+            <GiStickyBoot />
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Kaymayan ve Uzun Ömürlü Baskı</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                cumque tempore est ab possimus quisquam reiciendis tempora
+                animi! Quaerat, saepe?
+              </p>
+            </div>
+          </div><div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+            <FaCarBattery />
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Yedek Lityum Bataryalar ile 9 Saate Varan Durmadan Çalışma Süreleri</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                cumque tempore est ab possimus quisquam reiciendis tempora
+                animi! Quaerat, saepe?
+              </p>
+            </div>
+          </div><div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+              <MdTouchApp />
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">4&quot; Dokunmatik Panel (PLT Serisi) </h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                cumque tempore est ab possimus quisquam reiciendis tempora
+                animi! Quaerat, saepe?
+              </p>
+            </div>
+          </div><div className="flex items-start gap-4">
+            <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+            <FaShieldAlt />
+            </span>
+
+            <div>
+              <h2 className="text-lg font-bold">Mekanik Darbelere Karşı, Motor Koruma Sistemi</h2>
 
               <p className="mt-1 text-sm text-gray-300">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
