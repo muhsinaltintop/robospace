0423fb5c1415ce0779748154d7828bf2961bde58
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-28 22:36:54 +0100

    speed insight added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +15,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+
         <Header />
         <div className="mx-10">
-
         {children}
         </div>
-
         <Footer />
+        <SpeedInsights/>
+        
       </body>
     </html>
   );
